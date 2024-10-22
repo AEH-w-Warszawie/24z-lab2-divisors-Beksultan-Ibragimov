89b308b325b3fca4c65d9b8056d796b14d14ed98
Beksultan Ibragimov <ibragbe1113_aehit@students.vizja.pl>
2024-10-22 02:51:42 +0200

    Update DivisorsLib.cpp

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -1,14 +1,21 @@
 #include <vector>
-#include "Divisors.h"
+#include <cmath>
 
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
-	//write more fast algorithm( lecture)
+	//write more fast algorithm(whatch the lecture) [DONE!]
 	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
+	for (unsigned long long i = 1; i <= std::sqrt(number); ++i)
 	{
 		if (number % i == 0)
+		{
 			result.push_back(i);
+			if (i != number / i)
+			{
+				result.push_back(number / i);
+			}
+		}
 	}
 	return result;
 }
+//Beksultan Ibragimov 56916
